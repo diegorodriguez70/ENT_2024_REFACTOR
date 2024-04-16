66f37395b6155bc8487be265936b1bfb99c5b4d5
Mañana <Maana@AULA8-18>
2024-04-16 09:43:07 +0200

    Ejercicio 1, solucion. Programa arreglado

--- src/ejercicio1/Calculadora.java
+++ src/ejercicio1/Calculadora.java
@@ -1,29 +1,29 @@
 package ejercicio1;
 
 public class Calculadora {
-	public int sumar(int a, int b) {
-		int suma = a + b;
+	public int sumar(int sumando1, int sumando2) {
+		int suma = sumando1 + sumando2;
 		return suma;
 	}
 
-	public int restar(int a, int b) {
-		int resta = a - b;
+	public int restar(int minuendo, int sustraendo) {
+		int resta = minuendo - sustraendo;
 		return resta;
 	}
 
-	public int multiplicar(int a, int b) {
-		int multiplicacion = a * b;
+	public int multiplicar(int factor1, int factor2) {
+		int multiplicacion = factor1 * factor2;
 		return multiplicacion;
 	}
 
-	public int dividir(int a, int b) {
+	public int dividir(int dividendo, int divisor) {
 		try {
-			int division = a / b;
+			int division = dividendo / divisor;
 			return division;
 
 		} catch (Exception e) {
 
-			System.out.println("No se puede dividir entre 0");
+			System.out.print(e.getMessage());
 			return 0;
 		}
 	}
